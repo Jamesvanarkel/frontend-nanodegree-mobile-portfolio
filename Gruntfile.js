4aad0462d791661ce168a351558cc5aba1d59806
James van Arkel <j.van.arkel@brickparking.com>
2015-06-15 14:17:23 +0200

    First commit + Grunt + optimisations to index and projects (currently busy with pizza page)

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          src: ['css/*.css', '!*.min.css'],
+          ext: '.min.css'
+        }]
+      }
+    },
+    htmlmin: {
+      dist: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {
+          'index.html': 'index-source.html'
+        }
+      }
+    },
+    responsive_images: {
+      target: {
+        options: {
+          sizes: [{
+            width: 100,
+            name: "small"
+          }]
+        },
+        files: [{
+          expand: true,
+          src: ['views/images/pizzeria.jpg']
+        }]
+      }
+    },
+  });
+
+  // Load tasks.
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-responsive-images');
+  
+  // Register tasks.
+  grunt.registerTask('default', ['cssmin', 'htmlmin']);
+  grunt.registerTask('images', ['responsive_images']);
+};
